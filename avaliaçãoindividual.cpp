948d9775a359202a550ee179668267c23c7f7138
AlvaroMaciel93 <142815803+AlvaroMaciel93@users.noreply.github.com>
2023-09-20 09:10:07 -0300

    Update avaliaçãoindividual.cpp
    Correção de alguns erros e inclusão da pergunta na função de alterar, tanto no módulo de Passageiro quanto no módulo de Roteiro.

--- avaliaçãoindividual.cpp
+++ avaliaçãoindividual.cpp
@@ -16,7 +16,7 @@ struct Roteiro {
     string origem;
     string destino;
     string data_hora_prev;
-    int duracao_prev;
+    string duracao_prev;
 };
 
 int menuPassageiros() {
@@ -38,9 +38,10 @@ void incluirPassageiro(vector<Passageiro>& passageiros) {
     cout << "CPF: ";
     cin >> novoPassageiro.cpf;
     cout << "Nome: ";
-    cin >> novoPassageiro.nome;
+    cin.ignore();
+    getline(cin, novoPassageiro.nome);
     cout << "Data de Nascimento: ";
-    cin >> novoPassageiro.dataNasc;
+    getline(cin, novoPassageiro.dataNasc);
     cout << "Número de autorização: ";
     cin >> novoPassageiro.numeroAutorizacao;
     passageiros.push_back(novoPassageiro);
@@ -70,13 +71,30 @@ void alterarPassageiro(vector<Passageiro>& passageiros) {
     
     for (auto& passageiro : passageiros) {
         if (passageiro.cpf == cpf) {
-            cout << "Novos dados do passageiro:" << endl;
-            cout << "Nome: ";
-            cin >> passageiro.nome;
-            cout << "Data de Nascimento: ";
-            cin >> passageiro.dataNasc;
-            cout << "Número de autorização: ";
-            cin >> passageiro.numeroAutorizacao;
+            cout << "Deseja alterar o Nome? (S/N): ";
+            char resposta;
+            cin >> resposta;
+            if (resposta == 'S' || resposta == 's') {
+                cout << "Novo Nome: ";
+                cin.ignore();
+                getline(cin, passageiro.nome);
+            }
+
+            cout << "Deseja alterar a Data de Nascimento? (S/N): ";
+            cin >> resposta;
+            if (resposta == 'S' || resposta == 's') {
+                cout << "Nova Data de Nascimento: ";
+                cin.ignore();
+                getline(cin, passageiro.dataNasc);
+            }
+
+            cout << "Deseja alterar o Número de Autorização? (S/N): ";
+            cin >> resposta;
+            if (resposta == 'S' || resposta == 's') {
+                cout << "Novo Número de Autorização: ";
+                cin >> passageiro.numeroAutorizacao;
+            }
+
             cout << "Dados do passageiro alterados com sucesso!" << endl;
             return;
         }
@@ -84,6 +102,7 @@ void alterarPassageiro(vector<Passageiro>& passageiros) {
     
     cout << "Passageiro não encontrado." << endl;
 }
+
 
 void listarPassageiros(const vector<Passageiro>& passageiros) {
     cout << "\nLista de Passageiros:" << endl;
@@ -133,13 +152,14 @@ void incluirRoteiro(vector<Roteiro>& roteiros) {
     cout << "Código: ";
     cin >> novoRoteiro.codigo;
     cout << "Origem: ";
-    cin >> novoRoteiro.origem;
+    cin.ignore();
+    getline(cin, novoRoteiro.origem);
     cout << "Destino: ";
-    cin >> novoRoteiro.destino;
+    getline(cin, novoRoteiro.destino);
     cout << "Data e Hora previstas: ";
-    cin >> novoRoteiro.data_hora_prev;
+    getline(cin, novoRoteiro.data_hora_prev);
     cout << "Duração prevista: ";
-    cin >> novoRoteiro.duracao_prev;
+    getline(cin, novoRoteiro.duracao_prev);
     roteiros.push_back(novoRoteiro);
     cout << "Roteiro incluído com sucesso!" << endl;
 }
@@ -162,20 +182,44 @@ void excluirRoteiro(vector<Roteiro>& roteiros) {
 
 void alterarRoteiro(vector<Roteiro>& roteiros) {
     int codigo;
-    cout << "Digite o Código do roteiro a ser alterado: ";
+    cout << "Digite o código do roteiro a ser alterado: ";
     cin >> codigo;
     
     for (auto& roteiro : roteiros) {
         if (roteiro.codigo == codigo) {
-            cout << "Novos dados do roteiro:" << endl;
-            cout << "Origem: ";
-            cin >> roteiro.origem;
-            cout << "Destino: ";
-            cin >> roteiro.destino;
-            cout << "Data e Hora previstas: ";
-            cin >> roteiro.data_hora_prev;
-            cout << "Duração prevista: ";
-            cin >> roteiro.duracao_prev;
+            cout << "Deseja alterar a origem? (S/N): ";
+            char resposta;
+            cin >> resposta;
+            if (resposta == 'S' || resposta == 's') {
+                cout << "Nova origem: ";
+                cin.ignore();
+                getline(cin, roteiro.origem);
+            }
+
+            cout << "Deseja alterar o destino? (S/N): ";
+            cin >> resposta;
+            if (resposta == 'S' || resposta == 's') {
+                cout << "Novo destino: ";
+                cin.ignore();
+                getline(cin, roteiro.destino);
+            }
+
+            cout << "Deseja alterar a data e hora previstas? (S/N): ";
+            cin >> resposta;
+            if (resposta == 'S' || resposta == 's') {
+                cout << "Nova data e hora previstas: ";
+                cin.ignore();
+                getline(cin, roteiro.data_hora_prev);
+            }
+
+            cout << "Deseja alterar a duração prevista? (S/N): ";
+            cin >> resposta;
+            if (resposta == 'S' || resposta == 's') {
+                cout << "Nova duração prevista: ";
+                cin.ignore();
+                getline(cin, roteiro.duracao_prev);
+            }
+
             cout << "Dados do roteiro alterados com sucesso!" << endl;
             return;
         }
@@ -190,13 +234,12 @@ void listarRoteiros(const vector<Roteiro>& roteiros) {
         cout << "Código: " << roteiro.codigo << endl;
         cout << "Origem: " << roteiro.origem << endl;
         cout << "Destino: " << roteiro.destino << endl;
-        cout << "Data e Hora previstas: " << roteiro.data_hora_prev << endl;
+        cout << "Data e hora previstas: " << roteiro.data_hora_prev << endl;
         cout << "Duração prevista: " << roteiro.duracao_prev << endl;
         cout << "--------------------------" << endl;
     }
 }
 
-// Função para localizar um roteiro por Código
 void localizarRoteiro(const vector<Roteiro>& roteiros) {
     int codigo;
     cout << "Digite o Código do roteiro a ser localizado: ";
@@ -212,7 +255,6 @@ void localizarRoteiro(const vector<Roteiro>& roteiros) {
             return;
         }
     }
-    
     cout << "Roteiro não encontrado." << endl;
 }
 
